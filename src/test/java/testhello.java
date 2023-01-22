c32f893ed5973462a9537dbba5cc62fd89247ec5
Dabeer Shaikh <dab8106@gmail.com>
2023-01-22 16:07:10 +0530

    Added Junit testing

--- src/test/java/testhello.java
+++ src/test/java/testhello.java
@@ -3,31 +3,31 @@ import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
-public class HelloWorldServletTest {
+public class HelloWorldJspTest {
     @Mock
     HttpServletRequest request;
 
     @Mock
     HttpServletResponse response;
 
+    @Mock
+    RequestDispatcher requestDispatcher;
+
     @Test
-    public void doGet() throws Exception {
+    public void testHelloJsp() throws Exception {
         StringWriter stringWriter = new StringWriter();
         PrintWriter writer = new PrintWriter(stringWriter);
 
         when(response.getWriter()).thenReturn(writer);
+        when(request.getRequestDispatcher("/index.jsp")).thenReturn(requestDispatcher);
 
-        new HelloWorldServlet().doGet(request, response);
-
-        assertEquals("<h1>Hello World!</h1>", stringWriter.toString());
-    }
-}
