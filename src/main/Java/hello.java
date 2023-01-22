c368b7c1c39f577798c59dd0a98c62e2fb505287
Dabeer Shaikh <dab8106@gmail.com>
2023-01-22 16:14:04 +0530

    reverted back without junit

--- src/main/Java/hello.java
+++
@@ -1,8 +0,0 @@
-import javax.servlet.*;
-import javax.servlet.http.*;
-
-public class HelloWorldServlet extends HttpServlet {
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.getRequestDispatcher("/index.jsp").forward(request, response);
-    }
-}
